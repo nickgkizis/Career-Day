19e75ed125f2c723f7284ce4f2ba3fd93bbbf84c
nick gkizis <nikolaosgkizis@gmail.com>
2025-02-19 22:10:25 +0200

    card reconstruction

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-document.getElementById("unlock-btn").addEventListener("click", function () {
-  const password = prompt("Enter the password to unlock the CV:");
+// document.getElementById("unlock-btn").addEventListener("click", function () {
+//   const password = prompt("Enter the password to unlock the CV:");
 
-  if (password === "1234") {
-    document.getElementById("cv-section").style.display = "block";
-    document.getElementById("unlock-btn").style.display = "none"; 
-  } else {
-    alert("Incorrect password!");
-  }
-});
+//   if (password === "1234") {
+//     document.getElementById("cv-section").style.display = "block";
+//     document.getElementById("unlock-btn").style.display = "none"; 
+//   } else {
+//     alert("Incorrect password!");
+//   }
+// });
